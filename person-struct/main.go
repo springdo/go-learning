57cbf07a0d925176b26e71be0cdd9995fdecc125
donal <donalspring@gmail.com>
2018-01-26 15:51:49 +0000

    Adding ref vs value diagram

--- person-struct/main.go
+++ person-struct/main.go
@@ -7,13 +7,33 @@ import (
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
+	// or if you don't want to have the name implied from the type
+	// contact   contactInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+//  *type here tells us the receiver is a pointer to a person
+func (p *person) updateName(newFirstName string) {
+	//  this won't work cuz without a receiver of (p *person) a new variable of p is copied from the caller. This means the update will not happen
+	// p.firstName = newFirstName
+
+	// to fix this we use pointer in the reciver type and the value of it in teh update call
+	// *p is the _value_ at the pointer's address
+	(*p).firstName = newFirstName
+
+	// KEY TO WORKING WITH POINTERS
+	// turn an _address_ into a _value_ with *address
+	// turn a _value_ into an _address_ with &value ie a pointer
 }
 
 func main() {
 	// create a new person obj
 
-	//  1, order of fileds in the strut
+	//  1, order of fileds in the strut (must do all fields)
 	// alex := person{"Alex", "Anderson"}
 
 	// 2, Key value setting. Not relying on order of struct
@@ -42,11 +62,20 @@ func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Jones",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:    "jim.jones@jj.com",
 			postCode: "ABC-123",
 		},
 	}
-	fmt.Printf("%+v", jim)
+	// &variable is a reference to the memory address of a varibale
+	jimPointer := &jim
+	// *pointer is the _value_ at the address being pointed to
+	jimPointer.print()
+
+	jim.print()
+	jim.updateName("James")
+	jim.print()
+
+	fmt.Println("\n****")
 
 }
